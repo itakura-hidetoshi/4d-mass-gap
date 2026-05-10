9584e0b67dc640cd3e0c29c32dea6f48dfd67da1
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-10 23:59:32 +0900

    add R1 theorem surface root

---
+++ MGAP4D/R1/TheoremSurface.lean
@@ -0,0 +1,5 @@
+import MGAP4D.R1.TheoremSurface.HilbertSurface
+import MGAP4D.R1.TheoremSurface.ExcitedSurface
+import MGAP4D.R1.TheoremSurface.InnerFunctionalSurface
+import MGAP4D.R1.TheoremSurface.ProjectionSurface
+import MGAP4D.R1.TheoremSurface.ExportSurface
