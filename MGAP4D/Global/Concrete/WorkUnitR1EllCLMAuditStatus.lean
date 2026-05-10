bd05388a5148b9dad0856eb10f4ed285806be8e9
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-10 22:27:07 +0900

    import global concrete R1 ell CLM audit status

---
+++ MGAP4D/Global/Concrete/WorkUnitR1EllCLMAuditStatus.lean
@@ -0,0 +1,21 @@
+namespace MGAP4D
+namespace Global
+namespace Concrete
+
+structure WUR1EllCLMGlobalAuditStatus where
+  selected_first : Prop
+  unlocks_kernel_path : Prop
+  no_release_effect : Prop
+  audit_required : Prop
+
+def WUR1EllCLMGlobalAuditStatus.ready (S : WUR1EllCLMGlobalAuditStatus) : Prop :=
+  S.selected_first ∧ S.unlocks_kernel_path ∧ S.no_release_effect ∧ S.audit_required
+
+theorem wu_r1_ell_clm_global_audit_status_pack
+    (S : WUR1EllCLMGlobalAuditStatus) :
+    S.ready ↔ S.selected_first ∧ S.unlocks_kernel_path ∧ S.no_release_effect ∧ S.audit_required := by
+  rfl
+
+end Concrete
+end Global
+end MGAP4D
