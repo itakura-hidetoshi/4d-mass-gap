241dfa2142bea9f1f2c887b7c59d739d00e1da4e
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-10 23:06:15 +0900

    add R2 Concrete import root

---
+++ MGAP4D/R2/Concrete.lean
@@ -0,0 +1,5 @@
+import MGAP4D.R2.Concrete.ReducingSubspaceStatus
+import MGAP4D.R2.Concrete.SelfAdjointRestrictionStatus
+import MGAP4D.R2.Concrete.ExcitedHamiltonianStatus
+import MGAP4D.R2.Concrete.SpectrumUnionStatus
+import MGAP4D.R2.Concrete.ExportStatus
