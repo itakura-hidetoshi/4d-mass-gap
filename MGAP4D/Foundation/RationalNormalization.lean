ff9e2392530f6cf4ef3f1cf63642e9028e8a041f
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-10 20:51:21 +0900

    add rational normalization module

---
+++ MGAP4D/Foundation/RationalNormalization.lean
@@ -0,0 +1,10 @@
+namespace MGAP4D
+namespace Foundation
+
+def normalizedGapValue : Rat := 33 / 20
+
+theorem normalizedGapValue_eq : normalizedGapValue = 33 / 20 := by
+  rfl
+
+end Foundation
+end MGAP4D
