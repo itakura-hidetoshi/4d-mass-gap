8d9ec94d393b9fbd44c920941a2bd8d14129ef19
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-11 07:46:26 +0900

    wire R6 request through main root

--- MGAP4D.lean
+++ MGAP4D.lean
@@ -19,6 +19,7 @@ import MGAP4D.ReplacementPass2
 import MGAP4D.ReplacementPass2Closure
 import MGAP4D.MathlibAdoptionGate
 import MGAP4D.MathlibAdoptionGate.R5SpectrumRequest
+import MGAP4D.MathlibAdoptionGate.R6IntervalRequest
 import MGAP4D.OperatorAPI
 import MGAP4D.R1.Concrete
 import MGAP4D.R1.TheoremSurface
