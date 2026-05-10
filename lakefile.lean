ada631bfcf951214c3b2f8eeb2923988caacfe8b
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-10 22:47:34 +0900

    make Lake project configuration GitHub-native

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,8 @@ import Lake
 open Lake DSL
 
 package «MGAP4D» where
-  -- add package configuration options here
+  srcDir := "."
+  moreLeanArgs := #["-DautoImplicit=false"]
 
 lean_lib «MGAP4D» where
-  -- add library configuration options here
+  roots := #[`MGAP4D]
