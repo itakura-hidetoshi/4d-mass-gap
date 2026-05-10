6f670bcbc076b3677380fd5f66f00a250bf87c71
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-10 22:58:49 +0900

    add R1 Concrete import root

---
+++ MGAP4D/R1/Concrete.lean
@@ -0,0 +1,5 @@
+import MGAP4D.R1.Concrete.HilbertScaffoldStatus
+import MGAP4D.R1.Concrete.ExcitedSubspaceStatus
+import MGAP4D.R1.Concrete.InnerFunctionalStatus
+import MGAP4D.R1.Concrete.ProjectionStatus
+import MGAP4D.R1.Concrete.ClosureTargetsStatus
