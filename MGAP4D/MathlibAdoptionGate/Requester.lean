fa45169d4eca447c22cf89ff11f51a753b33a5db
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-11 07:27:17 +0900

    add Mathlib adoption requesters

---
+++ MGAP4D/MathlibAdoptionGate/Requester.lean
@@ -0,0 +1,35 @@
+import MGAP4D.MathlibAdoptionGate.Policy
+
+namespace MGAP4D
+namespace MathlibAdoptionGate
+
+inductive MathlibRequester where
+  | r1Hilbert
+  | r2Restriction
+  | r4LowerBound
+  | r5Spectrum
+  | r6Interval
+  | r7AtomExact
+  deriving Repr, DecidableEq
+
+structure MathlibRequest where
+  requester : MathlibRequester
+  requestedImportGroup : String
+  reason : String
+  scoped : Bool
+  deriving Repr, DecidableEq
+
+def firstEligibleRequesters : List MathlibRequester := [
+  MathlibRequester.r1Hilbert,
+  MathlibRequester.r2Restriction,
+  MathlibRequester.r4LowerBound,
+  MathlibRequester.r5Spectrum,
+  MathlibRequester.r6Interval,
+  MathlibRequester.r7AtomExact
+]
+
+theorem firstEligibleRequesters_nonempty : firstEligibleRequesters.length > 0 := by
+  decide
+
+end MathlibAdoptionGate
+end MGAP4D
