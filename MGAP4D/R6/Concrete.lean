c71099906f4d1e696c0ab4db220aefcf539f0cc1
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-11 20:55:13 +0900

    wire R6 interval candidate bundle

--- MGAP4D/R6/Concrete.lean
+++ MGAP4D/R6/Concrete.lean
@@ -1,3 +1,4 @@
 import MGAP4D.R6.Concrete.GapIntervalStatus
 import MGAP4D.R6.Concrete.ExportStatus
 import MGAP4D.R6.Concrete.IntervalTheoremCandidate
+import MGAP4D.R6.Concrete.IntervalCandidateBundle
