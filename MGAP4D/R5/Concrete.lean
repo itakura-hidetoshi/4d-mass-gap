76e4c3080004eb215ed7df5d09501c976dc0864e
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-11 19:43:21 +0900

    wire R5 checklist

--- MGAP4D/R5/Concrete.lean
+++ MGAP4D/R5/Concrete.lean
@@ -3,3 +3,4 @@ import MGAP4D.R5.Concrete.InfimumStatus
 import MGAP4D.R5.Concrete.ExportStatus
 import MGAP4D.R5.Concrete.SpectrumTheoremCandidate
 import MGAP4D.R5.Concrete.SpectrumCandidateBundle
+import MGAP4D.R5.Concrete.SpectrumTheoremChecklist
