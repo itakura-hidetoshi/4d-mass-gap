fe43882c3bec5386903a1fa858dc9c21518c1a46
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-11 14:11:17 +0900

    wire R2 restriction theorem candidate

--- MGAP4D/R2/Concrete.lean
+++ MGAP4D/R2/Concrete.lean
@@ -3,3 +3,4 @@ import MGAP4D.R2.Concrete.SelfAdjointRestrictionStatus
 import MGAP4D.R2.Concrete.ExcitedHamiltonianStatus
 import MGAP4D.R2.Concrete.SpectrumUnionStatus
 import MGAP4D.R2.Concrete.ExportStatus
+import MGAP4D.R2.Concrete.RestrictionTheoremCandidate
