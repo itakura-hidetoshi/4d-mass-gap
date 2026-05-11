86e92ecc9180887910d5c5546a7ca5ef6bb75da9
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-11 12:42:27 +0900

    wire Mathlib dry run checklist

--- MGAP4D.lean
+++ MGAP4D.lean
@@ -24,6 +24,7 @@ import MGAP4D.MathlibAdoptionGate.R7AtomExactRequest
 import MGAP4D.MathlibAdoptionGate.RequestRegistry
 import MGAP4D.MathlibAdoptionGate.DryRunBranchPlan
 import MGAP4D.MathlibAdoptionGate.DryRunGate
+import MGAP4D.MathlibAdoptionGate.DryRunChecklist
 import MGAP4D.OperatorAPI
 import MGAP4D.R1.Concrete
 import MGAP4D.R1.Theorem
