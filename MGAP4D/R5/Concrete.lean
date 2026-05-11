29f52729fe6c536430d867197f01c6f71e49726e
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-11 19:32:01 +0900

    wire R5 spectrum candidate bundle

--- MGAP4D/R5/Concrete.lean
+++ MGAP4D/R5/Concrete.lean
@@ -2,3 +2,4 @@ import MGAP4D.R5.Concrete.SpectrumSetStatus
 import MGAP4D.R5.Concrete.InfimumStatus
 import MGAP4D.R5.Concrete.ExportStatus
 import MGAP4D.R5.Concrete.SpectrumTheoremCandidate
+import MGAP4D.R5.Concrete.SpectrumCandidateBundle
