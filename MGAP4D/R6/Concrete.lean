1de154832c8f46cf211b2953e0aa025eaeab1ab2
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-11 21:10:49 +0900

    wire R6 interval theorem checklist

--- MGAP4D/R6/Concrete.lean
+++ MGAP4D/R6/Concrete.lean
@@ -2,3 +2,4 @@ import MGAP4D.R6.Concrete.GapIntervalStatus
 import MGAP4D.R6.Concrete.ExportStatus
 import MGAP4D.R6.Concrete.IntervalTheoremCandidate
 import MGAP4D.R6.Concrete.IntervalCandidateBundle
+import MGAP4D.R6.Concrete.IntervalTheoremChecklist
