dbe90b0d3948fbb3e04858abbfe842762dcf8486
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-11 22:57:27 +0900

    wire R7 atom exact candidate bundle

--- MGAP4D/R7/Concrete.lean
+++ MGAP4D/R7/Concrete.lean
@@ -1,3 +1,4 @@
 import MGAP4D.R7.Concrete.AtomPersistenceStatus
 import MGAP4D.R7.Concrete.ExactGapStatus
 import MGAP4D.R7.Concrete.AtomExactTheoremCandidate
+import MGAP4D.R7.Concrete.AtomExactCandidateBundle
