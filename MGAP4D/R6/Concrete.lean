3edea798373e693d859ff50db3541196e0d3e0ff
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-11 21:19:40 +0900

    wire R6 interval proof obligation map

--- MGAP4D/R6/Concrete.lean
+++ MGAP4D/R6/Concrete.lean
@@ -3,3 +3,4 @@ import MGAP4D.R6.Concrete.ExportStatus
 import MGAP4D.R6.Concrete.IntervalTheoremCandidate
 import MGAP4D.R6.Concrete.IntervalCandidateBundle
 import MGAP4D.R6.Concrete.IntervalTheoremChecklist
+import MGAP4D.R6.Concrete.IntervalProofObligationMap
