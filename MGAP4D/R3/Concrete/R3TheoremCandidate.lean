0e7dc57240fdab582bb3d5bf0fd1764c585a8304
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-12 07:42:31 +0900

    add R3 theorem candidate alias

---
+++ MGAP4D/R3/Concrete/R3TheoremCandidate.lean
@@ -0,0 +1,33 @@
+import MGAP4D.R3.Concrete.ZeroFormKernelStatus
+import MGAP4D.MathlibAdoptionGate.R3ZeroKernelRequest
+
+namespace MGAP4D
+namespace R3
+namespace Concrete
+
+structure R3TheoremCandidate where
+  statusReady : Prop
+  shiftedCandidate : Prop
+  nonnegativeCandidate : Prop
+  zeroFormCandidate : Prop
+  sqrtRouteCandidate : Prop
+  domainBridgeDeferred : Prop
+  exportToR7Deferred : Prop
+  r3RequestReady : Prop
+  mathlibStillDeferred : Prop
+
+def R3TheoremCandidate.ready (C : R3TheoremCandidate) : Prop :=
+  C.statusReady ∧ C.shiftedCandidate ∧ C.nonnegativeCandidate ∧
+  C.zeroFormCandidate ∧ C.sqrtRouteCandidate ∧ C.domainBridgeDeferred ∧
+  C.exportToR7Deferred ∧ C.r3RequestReady ∧ C.mathlibStillDeferred
+
+theorem r3_theorem_candidate_pack
+    (C : R3TheoremCandidate) :
+    C.ready ↔ C.statusReady ∧ C.shiftedCandidate ∧ C.nonnegativeCandidate ∧
+      C.zeroFormCandidate ∧ C.sqrtRouteCandidate ∧ C.domainBridgeDeferred ∧
+      C.exportToR7Deferred ∧ C.r3RequestReady ∧ C.mathlibStillDeferred := by
+  rfl
+
+end Concrete
+end R3
+end MGAP4D
