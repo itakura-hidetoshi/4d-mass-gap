0674818c656dd34b674165d46fdcb8f1aba815ad
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-12 21:09:27 +0900

    wire R3 proof-obligation tightening pass 1 through R3 theorem root

--- MGAP4D/R3/Theorem.lean
+++ MGAP4D/R3/Theorem.lean
@@ -3,3 +3,4 @@ import MGAP4D.R3.Theorem.R3SkeletonBundle
 import MGAP4D.R3.Theorem.R3Milestone
 import MGAP4D.R3.Theorem.R3ClosureCandidate
 import MGAP4D.R3.Theorem.R3HardeningPass
+import MGAP4D.R3.Theorem.R3ProofObligationTighteningPass1
