a359a728f900cbaa99d238a17b97e271fc7686a1
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-12 18:37:27 +0900

    wire main adoption review gate through Mathlib adoption root

--- MGAP4D/MathlibAdoptionGate.lean
+++ MGAP4D/MathlibAdoptionGate.lean
@@ -8,3 +8,4 @@ import MGAP4D.MathlibAdoptionGate.DryRunHoldDraftPR6
 import MGAP4D.MathlibAdoptionGate.DryRunHoldDraftPR7
 import MGAP4D.MathlibAdoptionGate.DryRunHoldPR8
 import MGAP4D.MathlibAdoptionGate.DryRunSeriesReviewR1R7
+import MGAP4D.MathlibAdoptionGate.MainAdoptionReviewGate
