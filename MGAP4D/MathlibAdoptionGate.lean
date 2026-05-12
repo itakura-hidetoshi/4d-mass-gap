008f9f0298393e96c6a1cb8f227d5486b04d69bf
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-12 12:32:32 +0900

    update root

--- MGAP4D/MathlibAdoptionGate.lean
+++ MGAP4D/MathlibAdoptionGate.lean
@@ -1,7 +1,3 @@
 import MGAP4D.MathlibAdoptionGate.Policy
 import MGAP4D.MathlibAdoptionGate.Requester
 import MGAP4D.MathlibAdoptionGate.Gate
-import MGAP4D.MathlibAdoptionGate.R1HilbertRequest
-import MGAP4D.MathlibAdoptionGate.R2RestrictionRequest
-import MGAP4D.MathlibAdoptionGate.R3ZeroKernelRequest
-import MGAP4D.MathlibAdoptionGate.R4LowerBoundRequest
