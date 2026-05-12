1fcc77f0c6299d4c95ac80d8d74d7f23167cb9d2
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-12 09:39:59 +0900

    wire R3 concrete theorem path

--- MGAP4D/R3/Concrete.lean
+++ MGAP4D/R3/Concrete.lean
@@ -1,3 +1,7 @@
 import MGAP4D.R3.Concrete.ShiftedOperatorStatus
 import MGAP4D.R3.Concrete.ZeroFormKernelStatus
 import MGAP4D.R3.Concrete.ExportStatus
+import MGAP4D.R3.Concrete.R3TheoremCandidate
+import MGAP4D.R3.Concrete.R3CandidateBundle
+import MGAP4D.R3.Concrete.R3TheoremChecklist
+import MGAP4D.R3.Concrete.R3ProofObligationMap
