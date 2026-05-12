0270455cdc1abaaa979c1e68972ec31e9d0a6b72
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-12 19:22:06 +0900

    wire R5 closure candidate through R5 theorem root

--- MGAP4D/R5/Theorem.lean
+++ MGAP4D/R5/Theorem.lean
@@ -1,3 +1,4 @@
 import MGAP4D.R5.Theorem.SpectrumSkeleton
 import MGAP4D.R5.Theorem.SpectrumSkeletonBundle
 import MGAP4D.R5.Theorem.SpectrumMilestone
+import MGAP4D.R5.Theorem.SpectrumInfimumClosureCandidate
