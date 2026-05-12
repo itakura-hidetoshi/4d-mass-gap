6978d0add7d4939dc8211c8a85b53d65a32d474f
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-12 18:18:44 +0900

    dry-run R7 wire Mathlib sibling module

--- MGAP4D/R7/Theorem.lean
+++ MGAP4D/R7/Theorem.lean
@@ -1,3 +1,4 @@
 import MGAP4D.R7.Theorem.AtomExactSkeleton
 import MGAP4D.R7.Theorem.AtomExactSkeletonBundle
 import MGAP4D.R7.Theorem.AtomExactMilestone
+import MGAP4D.R7.Theorem.AtomExactMathlibDryRun
