0eddaead69750d635dc404cdacbf67bd07ade2bd
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-15 06:26:22 +0900

    adopt Mathlib dependency on exact-gap analytic branch

--- lakefile.lean
+++ lakefile.lean
@@ -5,5 +5,8 @@ package «MGAP4D» where
   srcDir := "."
   moreLeanArgs := #["-DautoImplicit=false"]
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "master"
+
 lean_lib «MGAP4D» where
   roots := #[`MGAP4D]
