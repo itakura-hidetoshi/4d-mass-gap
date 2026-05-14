8d6e2c60179e0ffe3952804ddf2d69cf9ddfb899
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-14 22:18:04 +0900

    wire public boundary theorem into root entrypoint

--- MGAP4D.lean
+++ MGAP4D.lean
@@ -120,3 +120,4 @@ import MGAP4D.FinalSpine
 import MGAP4D.R1R7TheoremObligationFinalSpineBridge
 import MGAP4D.SharpGapFinalSpineBridge
 import MGAP4D.ExactGapFinalSpineBridge
+import MGAP4D.Release.PublicBoundaryTheorem
