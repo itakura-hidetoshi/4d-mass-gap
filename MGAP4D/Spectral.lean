b170cbfd3c4d85b100cd8144598f1e712ed054f1
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-14 19:10:19 +0900

    wire spectral sector separation into spectral entrypoint

--- MGAP4D/Spectral.lean
+++ MGAP4D/Spectral.lean
@@ -3,5 +3,6 @@ import MGAP4D.Spectral.Gap
 import MGAP4D.Spectral.GapFormalization
 import MGAP4D.Spectral.PositiveGap
 import MGAP4D.Spectral.SectorBoundary
+import MGAP4D.Spectral.SectorSeparation
 import MGAP4D.Spectral.LowerBound
 import MGAP4D.Spectral.CoreCertificate
