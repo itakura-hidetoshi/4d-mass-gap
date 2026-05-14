c5f79cc489c5f407ba0f9ca7ee5c1ae2b0495178
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-14 18:55:59 +0900

    wire Hamiltonian normalization pre-release bridge into entrypoint

--- MGAP4D/Hamiltonian.lean
+++ MGAP4D/Hamiltonian.lean
@@ -2,3 +2,4 @@ import MGAP4D.Hamiltonian.Basic
 import MGAP4D.Hamiltonian.Physical
 import MGAP4D.Hamiltonian.Normalization
 import MGAP4D.Hamiltonian.SpectralNormalizationBridge
+import MGAP4D.Hamiltonian.NormalizationPreReleaseBridge
