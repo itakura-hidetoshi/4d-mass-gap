37ce7e8d5d4bd4096ee691ab05e4f20938f2860d
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-14 19:34:21 +0900

    wire observable spectral weight final bridge into Constructive entrypoint

--- MGAP4D/Constructive.lean
+++ MGAP4D/Constructive.lean
@@ -2,3 +2,4 @@ import MGAP4D.Constructive.Gap3320
 import MGAP4D.Constructive.Eigenvector
 import MGAP4D.Constructive.PlaquetteWitness
 import MGAP4D.Constructive.FinalTheorem
+import MGAP4D.Constructive.ObservableSpectralWeightBridge
