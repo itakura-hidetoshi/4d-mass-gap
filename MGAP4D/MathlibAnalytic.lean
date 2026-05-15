4170be782c08904fe86bb5737a593f263e5b183e
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-15 22:15:34 +0900

    wire spectral realization skeleton into analytic root

--- MGAP4D/MathlibAnalytic.lean
+++ MGAP4D/MathlibAnalytic.lean
@@ -36,3 +36,4 @@ import MGAP4D.MathlibAnalytic.HilbertInnerProductSkeleton
 import MGAP4D.MathlibAnalytic.HilbertSpaceInstanceSkeleton
 import MGAP4D.MathlibAnalytic.PhysicalUnboundedOperatorSkeleton
 import MGAP4D.MathlibAnalytic.ConcreteYangMillsHamiltonianSkeleton
+import MGAP4D.MathlibAnalytic.SpectralRealizationSkeleton
