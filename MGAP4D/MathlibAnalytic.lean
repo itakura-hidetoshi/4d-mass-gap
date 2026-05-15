8b560b7ef060247e8a3537d5b75059bcde492de6
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-15 14:59:09 +0900

    wire Mathlib PVM theorem into analytic root

--- MGAP4D/MathlibAnalytic.lean
+++ MGAP4D/MathlibAnalytic.lean
@@ -16,3 +16,4 @@ import MGAP4D.MathlibAnalytic.ExactGapPostInterfaceResidualMap
 import MGAP4D.MathlibAnalytic.HilbertRayleighQuotientTheorem
 import MGAP4D.MathlibAnalytic.SelfAdjointHPhysTheorem
 import MGAP4D.MathlibAnalytic.SpectralTheoremTheorem
+import MGAP4D.MathlibAnalytic.PVMTheoremTheorem
