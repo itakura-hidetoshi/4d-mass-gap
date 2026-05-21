89536700c754d10b172eaf3e4cb89aa3b2bf8935
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-21 12:10:47 +0900

    Wire R2m concrete zero compatibility import

--- MGAP4D/MathlibAnalytic.lean
+++ MGAP4D/MathlibAnalytic.lean
@@ -173,3 +173,4 @@ import MGAP4D.MathlibAnalytic.R2MPrefixQuotientAddWellDefined
 import MGAP4D.MathlibAnalytic.R2MPrefixQuotientSmulWellDefined
 import MGAP4D.MathlibAnalytic.R2MPrefixQuotientSmulOperation
 import MGAP4D.MathlibAnalytic.R2MPrefixQuotientSmulSeminormLaw
+import MGAP4D.MathlibAnalytic.R2MPrefixConcreteZeroCompatibility
