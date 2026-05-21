8a9db2cdb2efd5489c866c6b76d6797e48c6553b
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-21 11:51:27 +0900

    Wire R2m quotient scalar seminorm law import

--- MGAP4D/MathlibAnalytic.lean
+++ MGAP4D/MathlibAnalytic.lean
@@ -172,3 +172,4 @@ import MGAP4D.MathlibAnalytic.R2MPrefixQuotientNormDistanceBridge
 import MGAP4D.MathlibAnalytic.R2MPrefixQuotientAddWellDefined
 import MGAP4D.MathlibAnalytic.R2MPrefixQuotientSmulWellDefined
 import MGAP4D.MathlibAnalytic.R2MPrefixQuotientSmulOperation
+import MGAP4D.MathlibAnalytic.R2MPrefixQuotientSmulSeminormLaw
