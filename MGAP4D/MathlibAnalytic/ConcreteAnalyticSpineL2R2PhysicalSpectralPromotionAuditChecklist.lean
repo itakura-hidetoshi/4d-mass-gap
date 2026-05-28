b255acfe1fabac325553b7940a2098efcd6dda9c
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-28 13:18:46 +0900

    Connect completed diagonal eigenpair graph surface to R2 checklist

--- MGAP4D/MathlibAnalytic/ConcreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklist.lean
+++ MGAP4D/MathlibAnalytic/ConcreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklist.lean
@@ -1,5 +1,5 @@
 import MGAP4D.MathlibAnalytic.ConcreteAnalyticSpineL2R2ClosedOperatorTheorem
-import MGAP4D.MathlibAnalytic.ConcreteAnalyticSpineL2R2CompletedUnitEigenpairGrowthCertificate
+import MGAP4D.MathlibAnalytic.ConcreteAnalyticSpineL2R2CompletedDiagonalEigenpairGraphSurface
 
 namespace MGAP4D
 namespace MathlibAnalytic
@@ -25,6 +25,8 @@ structure ConcreteL2R2PhysicalSpectralPromotionAuditChecklist where
     concreteAnalyticSpineL2R2CompletedUnitVectorGrowthCertificateReady
   completedUnitEigenpairGrowthCertificateReady :
     concreteAnalyticSpineL2R2CompletedUnitEigenpairGrowthCertificateReady
+  completedDiagonalEigenpairGraphSurfaceReady :
+    concreteAnalyticSpineL2R2CompletedDiagonalEigenpairGraphSurfaceReady
   finiteSupportCoreReady : concreteL2R2FiniteSupportCoreReady
   graphNormFiniteSupportDensityReady : concreteL2R2GraphNormFiniteSupportDensityReady
   graphNormCoreReleaseReady : concreteL2R2GraphNormCoreReleaseReady
@@ -59,6 +61,8 @@ def concreteL2R2PhysicalSpectralPromotionAuditChecklist :
       concrete_analytic_spine_l2_r2_completed_unit_vector_growth_certificate_ready
     completedUnitEigenpairGrowthCertificateReady :=
       concrete_analytic_spine_l2_r2_completed_unit_eigenpair_growth_certificate_ready
+    completedDiagonalEigenpairGraphSurfaceReady :=
+      concrete_analytic_spine_l2_r2_completed_diagonal_eigenpair_graph_surface_ready
     finiteSupportCoreReady := concrete_analytic_spine_l2_r2_finite_support_core_ready
     graphNormFiniteSupportDensityReady := concrete_analytic_spine_l2_r2_graph_norm_finite_support_density_ready
     graphNormCoreReleaseReady := concrete_analytic_spine_l2_r2_graph_norm_core_release_ready
@@ -87,6 +91,7 @@ def concreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklistReady : Prop
   concreteAnalyticSpineL2R2CompletedUnitProbeOutputNormLowerBoundReady ∧
   concreteAnalyticSpineL2R2CompletedUnitVectorGrowthCertificateReady ∧
   concreteAnalyticSpineL2R2CompletedUnitEigenpairGrowthCertificateReady ∧
+  concreteAnalyticSpineL2R2CompletedDiagonalEigenpairGraphSurfaceReady ∧
   concreteL2R2FiniteSupportCoreReady ∧
   concreteL2R2GraphNormFiniteSupportDensityReady ∧
   concreteL2R2GraphNormCoreReleaseReady ∧
@@ -108,6 +113,7 @@ theorem concrete_analytic_spine_l2_r2_physical_spectral_promotion_audit_checklis
     concrete_analytic_spine_l2_r2_completed_unit_probe_output_norm_lower_bound_ready,
     concrete_analytic_spine_l2_r2_completed_unit_vector_growth_certificate_ready,
     concrete_analytic_spine_l2_r2_completed_unit_eigenpair_growth_certificate_ready,
+    concrete_analytic_spine_l2_r2_completed_diagonal_eigenpair_graph_surface_ready,
     concrete_analytic_spine_l2_r2_finite_support_core_ready,
     concrete_analytic_spine_l2_r2_graph_norm_finite_support_density_ready,
     concrete_analytic_spine_l2_r2_graph_norm_core_release_ready,
