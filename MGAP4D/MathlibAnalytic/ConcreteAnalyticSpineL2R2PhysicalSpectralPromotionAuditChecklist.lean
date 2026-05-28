9858d1935c31bf7cf6cace74d8ed80168d35b0eb
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-28 19:45:23 +0900

    Connect coordinate evaluation continuity to R2 checklist

--- MGAP4D/MathlibAnalytic/ConcreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklist.lean
+++ MGAP4D/MathlibAnalytic/ConcreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklist.lean
@@ -1,5 +1,5 @@
 import MGAP4D.MathlibAnalytic.ConcreteAnalyticSpineL2R2ClosedOperatorTheorem
-import MGAP4D.MathlibAnalytic.ConcreteAnalyticSpineL2R2NormCoordinateBridgeFromContinuity
+import MGAP4D.MathlibAnalytic.ConcreteAnalyticSpineL2R2CoordinateEvaluationContinuity
 
 namespace MGAP4D
 namespace MathlibAnalytic
@@ -15,8 +15,8 @@ structure ConcreteL2R2PhysicalSpectralPromotionAuditChecklist where
   concreteRealHilbertSpaceReady : concreteL2R2ConcreteRealHilbertSpaceReady
   denselyDefinedUnboundedOperatorReady : concreteL2R2DenselyDefinedOperatorReady
   diagonalOperatorEvidenceReady : concreteAnalyticSpineL2R2DiagonalOperatorEvidenceReady
-  normCoordinateBridgeFromContinuityReady :
-    concreteAnalyticSpineL2R2NormCoordinateBridgeFromContinuityReady
+  coordinateEvaluationContinuityReady :
+    concreteAnalyticSpineL2R2CoordinateEvaluationContinuityReady
   finiteSupportCoreReady : concreteL2R2FiniteSupportCoreReady
   graphNormFiniteSupportDensityReady : concreteL2R2GraphNormFiniteSupportDensityReady
   graphNormCoreReleaseReady : concreteL2R2GraphNormCoreReleaseReady
@@ -24,9 +24,7 @@ structure ConcreteL2R2PhysicalSpectralPromotionAuditChecklist where
   graphClosednessObligationPromotionReady : concreteL2R2GraphClosednessObligationPromotionReady
   graphClosureClosedTheoremReady : concreteL2R2GraphClosureClosedTheoremReady
   closedOperatorTheoremReady : concreteAnalyticSpineL2R2ClosedOperatorTheoremReady
-  boundaryNotCoordinateEvaluationContinuity : Prop
-  boundaryNotUnconditionalNormConvergenceToCoordinatewiseBridge : Prop
-  boundaryNotUnconditionalCompletedDiagonalClosedGraphTheorem : Prop
+  boundaryNotFullTopologicalClosedGraphAsIsClosed : Prop
   boundaryNotCompletedHilbertOperatorNormUnboundednessTheorem : Prop
   boundaryNotDiagonalGraphEqualsClosure : Prop
   boundaryNotOriginalDiagonalOperatorClosed : Prop
@@ -43,8 +41,8 @@ def concreteL2R2PhysicalSpectralPromotionAuditChecklist :
     concreteRealHilbertSpaceReady := concrete_analytic_spine_l2_r2_concrete_real_hilbert_space_ready
     denselyDefinedUnboundedOperatorReady := concrete_analytic_spine_l2_r2_densely_defined_operator_ready
     diagonalOperatorEvidenceReady := concrete_analytic_spine_l2_r2_diagonal_operator_evidence_ready
-    normCoordinateBridgeFromContinuityReady :=
-      concrete_analytic_spine_l2_r2_norm_coordinate_bridge_from_continuity_ready
+    coordinateEvaluationContinuityReady :=
+      concrete_analytic_spine_l2_r2_coordinate_evaluation_continuity_ready
     finiteSupportCoreReady := concrete_analytic_spine_l2_r2_finite_support_core_ready
     graphNormFiniteSupportDensityReady := concrete_analytic_spine_l2_r2_graph_norm_finite_support_density_ready
     graphNormCoreReleaseReady := concrete_analytic_spine_l2_r2_graph_norm_core_release_ready
@@ -52,9 +50,7 @@ def concreteL2R2PhysicalSpectralPromotionAuditChecklist :
     graphClosednessObligationPromotionReady := concrete_analytic_spine_l2_r2_graph_closedness_obligation_promotion_ready
     graphClosureClosedTheoremReady := concrete_analytic_spine_l2_r2_graph_closure_closed_theorem_ready
     closedOperatorTheoremReady := concrete_analytic_spine_l2_r2_closed_operator_theorem_ready
-    boundaryNotCoordinateEvaluationContinuity := True
-    boundaryNotUnconditionalNormConvergenceToCoordinatewiseBridge := True
-    boundaryNotUnconditionalCompletedDiagonalClosedGraphTheorem := True
+    boundaryNotFullTopologicalClosedGraphAsIsClosed := True
     boundaryNotCompletedHilbertOperatorNormUnboundednessTheorem := True
     boundaryNotDiagonalGraphEqualsClosure := True
     boundaryNotOriginalDiagonalOperatorClosed := True
@@ -70,7 +66,7 @@ def concreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklistReady : Prop
   concreteL2R2ConcreteRealHilbertSpaceReady ∧
   concreteL2R2DenselyDefinedOperatorReady ∧
   concreteAnalyticSpineL2R2DiagonalOperatorEvidenceReady ∧
-  concreteAnalyticSpineL2R2NormCoordinateBridgeFromContinuityReady ∧
+  concreteAnalyticSpineL2R2CoordinateEvaluationContinuityReady ∧
   concreteL2R2FiniteSupportCoreReady ∧
   concreteL2R2GraphNormFiniteSupportDensityReady ∧
   concreteL2R2GraphNormCoreReleaseReady ∧
@@ -78,7 +74,7 @@ def concreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklistReady : Prop
   concreteL2R2GraphClosednessObligationPromotionReady ∧
   concreteL2R2GraphClosureClosedTheoremReady ∧
   concreteAnalyticSpineL2R2ClosedOperatorTheoremReady ∧
-  True ∧ True ∧ True ∧ True ∧ True ∧ True ∧ True ∧ True ∧ True ∧ True ∧ True ∧ True
+  True ∧ True ∧ True ∧ True ∧ True ∧ True ∧ True ∧ True ∧ True ∧ True
 
 theorem concrete_analytic_spine_l2_r2_physical_spectral_promotion_audit_checklist_ready :
     concreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklistReady := by
@@ -87,7 +83,7 @@ theorem concrete_analytic_spine_l2_r2_physical_spectral_promotion_audit_checklis
     concrete_analytic_spine_l2_r2_concrete_real_hilbert_space_ready,
     concrete_analytic_spine_l2_r2_densely_defined_operator_ready,
     concrete_analytic_spine_l2_r2_diagonal_operator_evidence_ready,
-    concrete_analytic_spine_l2_r2_norm_coordinate_bridge_from_continuity_ready,
+    concrete_analytic_spine_l2_r2_coordinate_evaluation_continuity_ready,
     concrete_analytic_spine_l2_r2_finite_support_core_ready,
     concrete_analytic_spine_l2_r2_graph_norm_finite_support_density_ready,
     concrete_analytic_spine_l2_r2_graph_norm_core_release_ready,
@@ -95,8 +91,6 @@ theorem concrete_analytic_spine_l2_r2_physical_spectral_promotion_audit_checklis
     concrete_analytic_spine_l2_r2_graph_closedness_obligation_promotion_ready,
     concrete_analytic_spine_l2_r2_graph_closure_closed_theorem_ready,
     concrete_analytic_spine_l2_r2_closed_operator_theorem_ready,
-    trivial,
-    trivial,
     trivial,
     trivial,
     trivial,
