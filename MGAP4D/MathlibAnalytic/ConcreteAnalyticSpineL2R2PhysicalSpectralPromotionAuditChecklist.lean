a3621f4aa01c922ee367537ddde310ba369cf535
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-28 18:50:38 +0900

    Connect conditional closed graph theorem to R2 checklist

--- MGAP4D/MathlibAnalytic/ConcreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklist.lean
+++ MGAP4D/MathlibAnalytic/ConcreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklist.lean
@@ -1,5 +1,5 @@
 import MGAP4D.MathlibAnalytic.ConcreteAnalyticSpineL2R2ClosedOperatorTheorem
-import MGAP4D.MathlibAnalytic.ConcreteAnalyticSpineL2R2CompletedDiagonalClosedGraphRouteSurface
+import MGAP4D.MathlibAnalytic.ConcreteAnalyticSpineL2R2CompletedDiagonalConditionalClosedGraph
 
 namespace MGAP4D
 namespace MathlibAnalytic
@@ -15,8 +15,8 @@ structure ConcreteL2R2PhysicalSpectralPromotionAuditChecklist where
   concreteRealHilbertSpaceReady : concreteL2R2ConcreteRealHilbertSpaceReady
   denselyDefinedUnboundedOperatorReady : concreteL2R2DenselyDefinedOperatorReady
   diagonalOperatorEvidenceReady : concreteAnalyticSpineL2R2DiagonalOperatorEvidenceReady
-  completedDiagonalClosedGraphRouteSurfaceReady :
-    concreteAnalyticSpineL2R2CompletedDiagonalClosedGraphRouteSurfaceReady
+  completedDiagonalConditionalClosedGraphReady :
+    concreteAnalyticSpineL2R2CompletedDiagonalConditionalClosedGraphReady
   finiteSupportCoreReady : concreteL2R2FiniteSupportCoreReady
   graphNormFiniteSupportDensityReady : concreteL2R2GraphNormFiniteSupportDensityReady
   graphNormCoreReleaseReady : concreteL2R2GraphNormCoreReleaseReady
@@ -25,7 +25,7 @@ structure ConcreteL2R2PhysicalSpectralPromotionAuditChecklist where
   graphClosureClosedTheoremReady : concreteL2R2GraphClosureClosedTheoremReady
   closedOperatorTheoremReady : concreteAnalyticSpineL2R2ClosedOperatorTheoremReady
   boundaryNotNormConvergenceToCoordinatewiseBridge : Prop
-  boundaryNotCompletedDiagonalClosedGraphTheorem : Prop
+  boundaryNotUnconditionalCompletedDiagonalClosedGraphTheorem : Prop
   boundaryNotCompletedHilbertOperatorNormUnboundednessTheorem : Prop
   boundaryNotDiagonalGraphEqualsClosure : Prop
   boundaryNotOriginalDiagonalOperatorClosed : Prop
@@ -42,8 +42,8 @@ def concreteL2R2PhysicalSpectralPromotionAuditChecklist :
     concreteRealHilbertSpaceReady := concrete_analytic_spine_l2_r2_concrete_real_hilbert_space_ready
     denselyDefinedUnboundedOperatorReady := concrete_analytic_spine_l2_r2_densely_defined_operator_ready
     diagonalOperatorEvidenceReady := concrete_analytic_spine_l2_r2_diagonal_operator_evidence_ready
-    completedDiagonalClosedGraphRouteSurfaceReady :=
-      concrete_analytic_spine_l2_r2_completed_diagonal_closed_graph_route_surface_ready
+    completedDiagonalConditionalClosedGraphReady :=
+      concrete_analytic_spine_l2_r2_completed_diagonal_conditional_closed_graph_ready
     finiteSupportCoreReady := concrete_analytic_spine_l2_r2_finite_support_core_ready
     graphNormFiniteSupportDensityReady := concrete_analytic_spine_l2_r2_graph_norm_finite_support_density_ready
     graphNormCoreReleaseReady := concrete_analytic_spine_l2_r2_graph_norm_core_release_ready
@@ -52,7 +52,7 @@ def concreteL2R2PhysicalSpectralPromotionAuditChecklist :
     graphClosureClosedTheoremReady := concrete_analytic_spine_l2_r2_graph_closure_closed_theorem_ready
     closedOperatorTheoremReady := concrete_analytic_spine_l2_r2_closed_operator_theorem_ready
     boundaryNotNormConvergenceToCoordinatewiseBridge := True
-    boundaryNotCompletedDiagonalClosedGraphTheorem := True
+    boundaryNotUnconditionalCompletedDiagonalClosedGraphTheorem := True
     boundaryNotCompletedHilbertOperatorNormUnboundednessTheorem := True
     boundaryNotDiagonalGraphEqualsClosure := True
     boundaryNotOriginalDiagonalOperatorClosed := True
@@ -68,7 +68,7 @@ def concreteAnalyticSpineL2R2PhysicalSpectralPromotionAuditChecklistReady : Prop
   concreteL2R2ConcreteRealHilbertSpaceReady ∧
   concreteL2R2DenselyDefinedOperatorReady ∧
   concreteAnalyticSpineL2R2DiagonalOperatorEvidenceReady ∧
-  concreteAnalyticSpineL2R2CompletedDiagonalClosedGraphRouteSurfaceReady ∧
+  concreteAnalyticSpineL2R2CompletedDiagonalConditionalClosedGraphReady ∧
   concreteL2R2FiniteSupportCoreReady ∧
   concreteL2R2GraphNormFiniteSupportDensityReady ∧
   concreteL2R2GraphNormCoreReleaseReady ∧
@@ -85,7 +85,7 @@ theorem concrete_analytic_spine_l2_r2_physical_spectral_promotion_audit_checklis
     concrete_analytic_spine_l2_r2_concrete_real_hilbert_space_ready,
     concrete_analytic_spine_l2_r2_densely_defined_operator_ready,
     concrete_analytic_spine_l2_r2_diagonal_operator_evidence_ready,
-    concrete_analytic_spine_l2_r2_completed_diagonal_closed_graph_route_surface_ready,
+    concrete_analytic_spine_l2_r2_completed_diagonal_conditional_closed_graph_ready,
     concrete_analytic_spine_l2_r2_finite_support_core_ready,
     concrete_analytic_spine_l2_r2_graph_norm_finite_support_density_ready,
     concrete_analytic_spine_l2_r2_graph_norm_core_release_ready,
