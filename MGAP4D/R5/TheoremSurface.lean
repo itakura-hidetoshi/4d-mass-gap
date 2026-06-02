062fc459304b96b831ead0504b4bf0046532ae69
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-02 13:35:03 +0900

    Use R5 compact centered plaquette input import

--- MGAP4D/R5/TheoremSurface.lean
+++ MGAP4D/R5/TheoremSurface.lean
@@ -1,4 +1,4 @@
 import MGAP4D.R5.TheoremSurface.SpectrumSetSurface
 import MGAP4D.R5.TheoremSurface.InfimumSurface
 import MGAP4D.R5.TheoremSurface.ExportSurface
-import MGAP4D.R5.Theorem.ActualSpectralInputFromR4Bridge
+import MGAP4D.R5.Theorem.CompactCenteredPlaquetteInput
