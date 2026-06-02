9c19e726be7c6398bc9465f79e3a39f0ee65565c
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-02 13:12:48 +0900

    Import R4 self-adjoint input bridge

--- MGAP4D/R4/TheoremSurface.lean
+++ MGAP4D/R4/TheoremSurface.lean
@@ -1,3 +1,4 @@
 import MGAP4D.R4.TheoremSurface.LowerBoundSurface
 import MGAP4D.R4.TheoremSurface.BridgeSurface
 import MGAP4D.R4.TheoremSurface.ExportSurface
+import MGAP4D.R4.Theorem.SelfAdjointInputBridge
