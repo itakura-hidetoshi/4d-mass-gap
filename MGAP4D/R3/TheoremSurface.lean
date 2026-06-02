063ff065fa73a501fc462f4b9c9db2b0ddb3d3ec
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-02 13:59:16 +0900

    Import R3 self-adjoint closure bridge

--- MGAP4D/R3/TheoremSurface.lean
+++ MGAP4D/R3/TheoremSurface.lean
@@ -1,3 +1,4 @@
 import MGAP4D.R3.TheoremSurface.ShiftedSurface
 import MGAP4D.R3.TheoremSurface.ZeroFormSurface
 import MGAP4D.R3.TheoremSurface.ExportSurface
+import MGAP4D.R3.Theorem.SelfAdjointClosureBridge
