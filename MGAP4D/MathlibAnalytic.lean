aee45d0a622809f8a4c0c9e1271f3599fd7903ec
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-12 11:18:24 +0900

    Import axiomatic Yang-Mills mass gap closure in MathlibAnalytic root

--- MGAP4D/MathlibAnalytic.lean
+++ MGAP4D/MathlibAnalytic.lean
@@ -1,4 +1,5 @@
 import MGAP4D.MathlibAnalytic.Basic
+import MGAP4D.MathlibAnalytic.AxiomaticYangMillsMassGapClosure
 import MGAP4D.MathlibAnalytic.FinalPhysicalHilbertCarrierCore
 import MGAP4D.MathlibAnalytic.ExactGapReal
 import MGAP4D.MathlibAnalytic.GapInfimumReal
