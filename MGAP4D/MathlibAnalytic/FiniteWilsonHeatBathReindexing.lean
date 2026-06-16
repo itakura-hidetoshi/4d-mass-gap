99f5098b14ce993603fe9d3eebe6ebaa3d156e0a
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-16 22:40:10 +0900

    Factor heat-bath reindexing through Equiv.sum_comp

--- MGAP4D/MathlibAnalytic/FiniteWilsonHeatBathReindexing.lean
+++ MGAP4D/MathlibAnalytic/FiniteWilsonHeatBathReindexing.lean
@@ -22,18 +22,25 @@ theorem finite_lattice_singleLinkHeatBath_reversible_product_sum
           (L.singleLinkConditionalPMF x.1 e x.2).toReal *
           f x.1 * g (L.replaceLink x.1 e x.2) := by
   classical
-  refine Fintype.sum_equiv (L.singleLinkUpdateSwapEquiv e) _ _ ?_
-  rintro ⟨A, h⟩
-  change L.gibbsProbabilityReal A *
-      (L.singleLinkConditionalPMF A e h).toReal *
-      f (L.replaceLink A e h) * g A =
-    L.gibbsProbabilityReal (L.replaceLink A e h) *
-      (L.singleLinkConditionalPMF
-        (L.replaceLink A e h) e (A e)).toReal *
-      f (L.replaceLink A e h) *
-      g (L.replaceLink (L.replaceLink A e h) e (A e))
-  rw [finite_lattice_singleLinkHeatBath_detailedBalance_real]
-  simp
+  let τ := L.singleLinkUpdateSwapEquiv e
+  calc
+    _ = ∑ x : L.Configuration × L.Gauge,
+        L.gibbsProbabilityReal (τ x).1 *
+          (L.singleLinkConditionalPMF (τ x).1 e (τ x).2).toReal *
+          f (τ x).1 * g (L.replaceLink (τ x).1 e (τ x).2) := by
+      apply Finset.sum_congr rfl
+      rintro ⟨A, h⟩ _
+      change L.gibbsProbabilityReal A *
+          (L.singleLinkConditionalPMF A e h).toReal *
+          f (L.replaceLink A e h) * g A =
+        L.gibbsProbabilityReal (L.replaceLink A e h) *
+          (L.singleLinkConditionalPMF
+            (L.replaceLink A e h) e (A e)).toReal *
+          f (L.replaceLink A e h) *
+          g (L.replaceLink (L.replaceLink A e h) e (A e))
+      rw [finite_lattice_singleLinkHeatBath_detailedBalance_real]
+      simp
+    _ = _ := τ.sum_comp _
 
 end
 
