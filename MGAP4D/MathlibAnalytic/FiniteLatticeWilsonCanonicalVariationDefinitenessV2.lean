5f4a0eca196b1f563021c6edf974d1519a9425fd
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-17 23:49:28 +0900

    Wire finite edge decidable equality in definiteness V2

--- MGAP4D/MathlibAnalytic/FiniteLatticeWilsonCanonicalVariationDefinitenessV2.lean
+++ MGAP4D/MathlibAnalytic/FiniteLatticeWilsonCanonicalVariationDefinitenessV2.lean
@@ -1,1 +1,15 @@
 import MGAP4D.MathlibAnalytic.FiniteLatticeWilsonCanonicalCenteredVariationProfile
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+noncomputable instance finiteLatticeWilsonEdgeDecidableEq
+    (L : FiniteLatticeWilsonSystem) : DecidableEq L.Edge :=
+  Classical.decEq L.Edge
+
+end
+
+end MathlibAnalytic
+end MGAP4D
