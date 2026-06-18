d83b398c34dd44c771420a866106a02f374b99d6
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-18 16:55:26 +0900

    Compile compact core scale fields

--- MGAP4D/MathlibAnalytic/PhysicalYangMillsCompactContainmentLimitCompileSmoke.lean
+++ MGAP4D/MathlibAnalytic/PhysicalYangMillsCompactContainmentLimitCompileSmoke.lean
@@ -7,23 +7,12 @@ open Filter MeasureTheory Set
 
 noncomputable section
 
-/-- Compile gate for the quantitative compact-core data. -/
-theorem physical_compact_containment_tail_compile_smoke
+theorem physical_compact_core_tail_compile_smoke
     {E : PhysicalFourDimensionalYangMillsLatticeEmbedding}
-    (C : PhysicalYangMillsUniformCompactContainment E) :
+    (C : PhysicalYangMillsCompactCoreScale E) :
     Tendsto C.tailBound atTop (nhds 0) :=
   C.tailBound_tendsto_zero
 
-/-- Compile gate for the uniform complement-mass estimate. -/
-theorem physical_compact_containment_bound_compile_smoke
-    {E : PhysicalFourDimensionalYangMillsLatticeEmbedding}
-    (C : PhysicalYangMillsUniformCompactContainment E)
-    (latticeScale compactScale : ℕ) :
-    E.embeddedMeasure latticeScale (C.compactCore compactScale)ᶜ ≤
-      C.tailBound compactScale :=
-  C.measure_compl_le latticeScale compactScale
-
 end
-
 end MathlibAnalytic
 end MGAP4D
