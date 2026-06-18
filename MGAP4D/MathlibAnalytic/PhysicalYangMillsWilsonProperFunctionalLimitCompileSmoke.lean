924c402d5860b0925b3dcbbb8eb417979f83fdb0
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-19 05:54:44 +0900

    Expose the special-unitary gauge weak-limit frontier

--- MGAP4D/MathlibAnalytic/PhysicalYangMillsWilsonProperFunctionalLimitCompileSmoke.lean
+++ MGAP4D/MathlibAnalytic/PhysicalYangMillsWilsonProperFunctionalLimitCompileSmoke.lean
@@ -1,4 +1,5 @@
 import MGAP4D.MathlibAnalytic.PhysicalYangMillsWilsonProperFunctionalLimit
+import MGAP4D.MathlibAnalytic.PhysicalYangMillsWilsonSpecialUnitaryGaugeFamily
 
 namespace MGAP4D
 namespace MathlibAnalytic
