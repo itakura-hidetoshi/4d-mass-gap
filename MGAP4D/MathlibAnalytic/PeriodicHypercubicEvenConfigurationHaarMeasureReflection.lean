cab5e9719d8f6ad9b6bd552b47fe5ca4f848205a
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-21 22:13:36 +0900

    Start full configuration Haar reflection invariance

---
+++ MGAP4D/MathlibAnalytic/PeriodicHypercubicEvenConfigurationHaarMeasureReflection.lean
@@ -0,0 +1,13 @@
+import MGAP4D.MathlibAnalytic.PeriodicHypercubicEvenConfigurationHaarReflection
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+open MeasureTheory
+
+noncomputable section
+
+end
+
+end MathlibAnalytic
+end MGAP4D
