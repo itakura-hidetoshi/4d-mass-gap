111f3f7073bf544f4e7b1a1ace63217ecc165c60
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-29 19:33:17 +0900

    Define periodic SU(N) continuum excitation resolvent

--- MGAP4D/MathlibAnalytic/PeriodicHypercubicSpecialUnitaryUniformImplicitEulerOSResolventCoreL2.lean
+++ MGAP4D/MathlibAnalytic/PeriodicHypercubicSpecialUnitaryUniformImplicitEulerOSResolventCoreL2.lean
@@ -6,6 +6,37 @@ namespace MathlibAnalytic
 
 noncomputable section
 
+namespace PeriodicHypercubicSpecialUnitaryUniformDobrushinFamilyData
+
+variable
+    {sideLength : ℕ → ℕ}
+    {sideLength_pos : ∀ n, 0 < sideLength n}
+    {N : ℕ}
+    {hN : 0 < N}
+    [Nontrivial (Matrix.specialUnitaryGroup (Fin N) ℂ)]
+    {beta : ℕ → ℝ}
+    {beta_nonneg : ∀ n, 0 ≤ beta n}
+
+variable
+    {S : PhysicalFourDimensionalYangMillsSymmetryLimit}
+    {W : PhysicalYangMillsGaugeInvariantOSReflectionData S}
+    {P : W.OSPreHilbertData}
+    {T : P.StronglyContinuousPhysicalSemigroup}
+
+noncomputable def periodicVacuumOrthogonalRealResolvent
+    (D : PeriodicHypercubicSpecialUnitaryUniformDobrushinFamilyData
+      sideLength sideLength_pos N hN beta beta_nonneg)
+    (B : D.UniformImplicitEulerOSBridge)
+    (hP : P.IsNormalized)
+    (hSelf : IsSelfAdjoint T.closedRightHamiltonian)
+    {lambda : ℝ}
+    (hlambda : lambda < 1 - D.coefficientBound) :
+    P.VacuumOrthogonalHilbert →L[ℝ] P.VacuumOrthogonalHilbert :=
+  B.vacuumOrthogonalRealResolvent hP hSelf (by
+    simpa only [toUniformFamily_gap] using hlambda)
+
+end PeriodicHypercubicSpecialUnitaryUniformDobrushinFamilyData
+
 end
 
 end MathlibAnalytic
