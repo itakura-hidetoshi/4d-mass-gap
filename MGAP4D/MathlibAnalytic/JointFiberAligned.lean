66e7f35d4db4280dcdad7624b80af42cb1f21850
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-29 20:23:13 +0900

    Open aligned joint fiber module

---
+++ MGAP4D/MathlibAnalytic/JointFiberAligned.lean
@@ -0,0 +1,14 @@
+import MGAP4D.MathlibAnalytic.ContinuousCompactOrientedGaugeWilsonHeatBathFiberIntegral
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+open MeasureTheory
+open scoped ENNReal
+
+noncomputable section
+
+end
+
+end MathlibAnalytic
+end MGAP4D
