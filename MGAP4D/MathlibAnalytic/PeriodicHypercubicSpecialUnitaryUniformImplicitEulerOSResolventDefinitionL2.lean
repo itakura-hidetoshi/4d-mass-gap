04532f3f513dfa7ab0f6f947215d76ce5dd7f92f
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-29 19:47:03 +0900

    Open periodic SU(N) resolvent definition module

---
+++ MGAP4D/MathlibAnalytic/PeriodicHypercubicSpecialUnitaryUniformImplicitEulerOSResolventDefinitionL2.lean
@@ -0,0 +1,12 @@
+import MGAP4D.MathlibAnalytic.PeriodicHypercubicSpecialUnitaryUniformImplicitEulerOSGapL2
+import MGAP4D.MathlibAnalytic.ContinuousCompactOrientedGaugeWilsonUniformImplicitEulerOSRealResolventL2
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+end
+
+end MathlibAnalytic
+end MGAP4D
