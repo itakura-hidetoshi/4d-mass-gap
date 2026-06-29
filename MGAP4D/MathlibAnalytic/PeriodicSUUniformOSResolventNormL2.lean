88f1e34aadb3a19486a72974de53812f48e51a4b
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-29 19:49:18 +0900

    Open periodic SU(N) resolvent norm module

---
+++ MGAP4D/MathlibAnalytic/PeriodicSUUniformOSResolventNormL2.lean
@@ -0,0 +1,11 @@
+import MGAP4D.MathlibAnalytic.PeriodicHypercubicSpecialUnitaryUniformImplicitEulerOSResolventDefinitionL2
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+end
+
+end MathlibAnalytic
+end MGAP4D
