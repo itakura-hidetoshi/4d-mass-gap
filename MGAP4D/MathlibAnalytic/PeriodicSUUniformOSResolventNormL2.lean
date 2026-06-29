3990711875a34ac935ee76f851d834ebcf59e28d
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-29 19:49:50 +0900

    Declare periodic SU(N) resolvent norm parameters

--- MGAP4D/MathlibAnalytic/PeriodicSUUniformOSResolventNormL2.lean
+++ MGAP4D/MathlibAnalytic/PeriodicSUUniformOSResolventNormL2.lean
@@ -5,6 +5,19 @@ namespace MathlibAnalytic
 
 noncomputable section
 
+namespace PeriodicHypercubicSpecialUnitaryUniformDobrushinFamilyData
+
+variable {sideLength : ℕ → ℕ} {sideLength_pos : ∀ n, 0 < sideLength n}
+variable {N : ℕ} {hN : 0 < N}
+variable [Nontrivial (Matrix.specialUnitaryGroup (Fin N) ℂ)]
+variable {beta : ℕ → ℝ} {beta_nonneg : ∀ n, 0 ≤ beta n}
+variable {S : PhysicalFourDimensionalYangMillsSymmetryLimit}
+variable {W : PhysicalYangMillsGaugeInvariantOSReflectionData S}
+variable {P : W.OSPreHilbertData}
+variable {T : P.StronglyContinuousPhysicalSemigroup}
+
+end PeriodicHypercubicSpecialUnitaryUniformDobrushinFamilyData
+
 end
 
 end MathlibAnalytic
