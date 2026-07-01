3e6c92041bf1f322cc607d6445cfdb59f92f02c2
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-02 08:36:14 +0900

    Start Prokhorov plaquette nondegeneracy proof

---
+++ MGAP4D/MathlibAnalytic/Z2PeriodicHypercubicBinaryPlaquetteProkhorovNondegeneracy.lean
@@ -0,0 +1,14 @@
+import MGAP4D.MathlibAnalytic.Z2PeriodicHypercubicBinaryPlaquetteProkhorov
+import Mathlib.Tactic
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+open Filter MeasureTheory
+
+noncomputable section
+
+end
+
+end MathlibAnalytic
+end MGAP4D
