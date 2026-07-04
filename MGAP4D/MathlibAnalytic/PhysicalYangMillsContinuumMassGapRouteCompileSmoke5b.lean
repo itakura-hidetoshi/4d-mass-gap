ed672279f9b642db9c0c50905c2e061018d98ebf
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-04 18:03:35 +0900

    Add route smoke final wrapper

---
+++ MGAP4D/MathlibAnalytic/PhysicalYangMillsContinuumMassGapRouteCompileSmoke5b.lean
@@ -0,0 +1,15 @@
+import MGAP4D.MathlibAnalytic.PhysicalYangMillsContinuumMassGapRouteCompileSmoke4b
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def routeSmoke5b : Prop := routeSmoke4b
+
+theorem routeSmoke5b_holds : routeSmoke5b := routeSmoke4b_holds
+
+end
+
+end MathlibAnalytic
+end MGAP4D
