81c06f124604bed963df7e52a6cfc308fca8169f
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-04 23:03:53 +0900

    Add R4 correlation structure compile smoke

---
+++ MGAP4D/MathlibAnalytic/EuclideanYangMillsR4CorrelationStructureCompileSmoke.lean
@@ -0,0 +1,18 @@
+import MGAP4D.MathlibAnalytic.EuclideanYangMillsR4CorrelationStructureClosure
+import Mathlib.Tactic
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def euclideanYangMillsR4CorrelationStructureCompileSmoke : Prop := True
+
+theorem euclideanYangMillsR4CorrelationStructureCompileSmoke_holds :
+    euclideanYangMillsR4CorrelationStructureCompileSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
