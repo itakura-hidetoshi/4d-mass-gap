925710987ea259cd687e59d06073ee08edbf8495
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-04 18:02:52 +0900

    Add third continuum route compile smoke

---
+++ MGAP4D/MathlibAnalytic/PhysicalYangMillsContinuumMassGapRouteCompileSmoke3.lean
@@ -0,0 +1,19 @@
+import MGAP4D.MathlibAnalytic.PhysicalYangMillsContinuumMassGapRouteCompileSmoke2
+import Mathlib.Tactic
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def continuumMassGapRouteCompileSmoke3 : Prop :=
+  continuumMassGapRouteCompileSmoke2
+
+theorem continuumMassGapRouteCompileSmoke3_holds :
+    continuumMassGapRouteCompileSmoke3 :=
+  continuumMassGapRouteCompileSmoke2_holds
+
+end
+
+end MathlibAnalytic
+end MGAP4D
