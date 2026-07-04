297f200068635e7d82f1d9fbfe72fda37508ab4e
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 08:59:43 +0900

    Add R4 quotient section representative projection smoke

---
+++ MGAP4D/MathlibAnalytic/EuclideanYangMillsR4HilbertReconstructionQuotientSectionRepresentativeProjectionSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.EuclideanYangMillsR4HilbertReconstructionQuotientSectionRepresentativeProjectionClosure
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def euclideanYangMillsR4SectionRepresentativeProjectionSmoke : Prop := True
+
+theorem euclideanYangMillsR4SectionRepresentativeProjectionSmoke_holds :
+    euclideanYangMillsR4SectionRepresentativeProjectionSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
