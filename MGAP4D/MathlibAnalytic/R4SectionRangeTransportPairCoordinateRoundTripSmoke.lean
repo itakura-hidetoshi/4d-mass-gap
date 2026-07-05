d59c6e4525cfeb56cba2cdf2e8e45259b583cce9
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 11:19:29 +0900

    Add R4 section range transport pair coordinate round-trip smoke

---
+++ MGAP4D/MathlibAnalytic/R4SectionRangeTransportPairCoordinateRoundTripSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateRoundTrip
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4SectionRangeTransportPairCoordinateRoundTripSmoke : Prop := True
+
+theorem r4SectionRangeTransportPairCoordinateRoundTripSmoke_holds :
+    r4SectionRangeTransportPairCoordinateRoundTripSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
