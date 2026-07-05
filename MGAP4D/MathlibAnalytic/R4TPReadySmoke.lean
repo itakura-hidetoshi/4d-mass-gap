e58a676b9c0f5df3b0fb4fd5659c12cf75e0b59d
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 17:33:06 +0900

    Add R4 ready API layer
    Add 30 theorem ready API layer.

---
+++ MGAP4D/MathlibAnalytic/R4TPReadySmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4TPReadyAPI
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4TPReadySmoke : Prop := True
+
+theorem r4TPReadySmoke_holds :
+    r4TPReadySmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
