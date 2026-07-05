611cde46dae486745083b3bd1dcf3c7fd2c0cbec
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 16:04:27 +0900

    Add R4 section range transport pair stable route API smoke file

---
+++ MGAP4D/MathlibAnalytic/R4SectionRangeTransportPairStableRouteAPISmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateRoundTripStableRouteAPI
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4SectionRangeTransportPairStableRouteAPISmoke : Prop := True
+
+theorem r4SectionRangeTransportPairStableRouteAPISmoke_holds :
+    r4SectionRangeTransportPairStableRouteAPISmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
