0e7dd8bb0e6e326f2e00cf7c8826d940b0019094
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 11:04:08 +0900

    Add R4 section range transport pair coordinate transport smoke

---
+++ MGAP4D/MathlibAnalytic/R4SectionRangeTransportPairCoordinateTransportSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateTransport
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4SectionRangeTransportPairCoordinateTransportSmoke : Prop := True
+
+theorem r4SectionRangeTransportPairCoordinateTransportSmoke_holds :
+    r4SectionRangeTransportPairCoordinateTransportSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
