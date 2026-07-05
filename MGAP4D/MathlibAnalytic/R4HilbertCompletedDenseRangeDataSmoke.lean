fa1db23153c9628a21e61417b79fa6703e5b17f2
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 20:51:10 +0900

    Add R4 dense range data
    Add completed dense range data.

---
+++ MGAP4D/MathlibAnalytic/R4HilbertCompletedDenseRangeDataSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertCompletedDenseRangeData
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertCompletedDenseRangeDataSmoke : Prop := True
+
+theorem r4HilbertCompletedDenseRangeDataSmoke_holds :
+    r4HilbertCompletedDenseRangeDataSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
