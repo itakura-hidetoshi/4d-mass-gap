8fdb309e0fb151c8f762394385535daadfa95a55
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 09:21:53 +0900

    Add R4 section range equivalence data smoke

---
+++ MGAP4D/MathlibAnalytic/R4SectionRangeEquivDataSmoke.lean
@@ -0,0 +1,16 @@
+import MGAP4D.MathlibAnalytic.EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeEquivDataClosure
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4SectionRangeEquivDataSmoke : Prop := True
+
+theorem r4SectionRangeEquivDataSmoke_holds : r4SectionRangeEquivDataSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
