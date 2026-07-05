54b7133c5ec3243a8d24929a07608ccc7a5a5b11
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 16:45:38 +0900

    Add R4 transport pair completion output API smoke file

---
+++ MGAP4D/MathlibAnalytic/R4TransportPairCompletionOutputAPISmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4TransportPairCompletionOutputAPI
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4TransportPairCompletionOutputAPISmoke : Prop := True
+
+theorem r4TransportPairCompletionOutputAPISmoke_holds :
+    r4TransportPairCompletionOutputAPISmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
