3b52be5e461c00c27eba0e28f0a0b372ce737ac2
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 21:37:54 +0900

    Add R4 canonical completion theorem API smoke file

---
+++ MGAP4D/MathlibAnalytic/R4HilbertCanonicalCompletionTheoremAPISmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertCanonicalCompletionTheoremAPI
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertCanonicalCompletionTheoremAPISmoke : Prop := True
+
+theorem r4HilbertCanonicalCompletionTheoremAPISmoke_holds :
+    r4HilbertCanonicalCompletionTheoremAPISmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
