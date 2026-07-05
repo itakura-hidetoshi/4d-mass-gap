8c45aa7e98782d650c0ae56e03ac2352ef9a8a42
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 21:53:41 +0900

    Add R4 standard completion identity API smoke file

---
+++ MGAP4D/MathlibAnalytic/R4HilbertStandardCompletionIdentityAPISmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertStandardCompletionIdentityAPI
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertStandardCompletionIdentityAPISmoke : Prop := True
+
+theorem r4HilbertStandardCompletionIdentityAPISmoke_holds :
+    r4HilbertStandardCompletionIdentityAPISmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
