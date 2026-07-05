49b8e5d4b592a78c273b4634c92e98077d8b9022
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 07:39:14 +0900

    Add R4 completed OS semigroup theorem smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertCompletedOSSemigroupTheoremAPISmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertCompletedOSSemigroupTheoremAPI
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertCompletedOSSemigroupTheoremAPISmoke : Prop := True
+
+theorem r4HilbertCompletedOSSemigroupTheoremAPISmoke_holds :
+    r4HilbertCompletedOSSemigroupTheoremAPISmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
