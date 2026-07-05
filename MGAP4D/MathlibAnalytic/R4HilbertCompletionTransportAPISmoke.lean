2b8434017a168fc0d628715715d2cc96ccc71cac
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 19:39:35 +0900

    Add R4 readiness transport APIs
    Add readiness and transport APIs.

---
+++ MGAP4D/MathlibAnalytic/R4HilbertCompletionTransportAPISmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertCompletionTransportAPI
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertCompletionTransportAPISmoke : Prop := True
+
+theorem r4HilbertCompletionTransportAPISmoke_holds :
+    r4HilbertCompletionTransportAPISmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
