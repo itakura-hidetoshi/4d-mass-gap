8a7e40c885839dd0a1d7e9000c5a747edea24c91
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 10:18:34 +0900

    Add R4 section range transport pair tuple smoke

---
+++ MGAP4D/MathlibAnalytic/R4SectionRangeTransportPairTupleSmoke.lean
@@ -0,0 +1,16 @@
+import MGAP4D.MathlibAnalytic.EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairTupleClosure
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4SectionRangeTransportPairTupleSmoke : Prop := True
+
+theorem r4SectionRangeTransportPairTupleSmoke_holds : r4SectionRangeTransportPairTupleSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
