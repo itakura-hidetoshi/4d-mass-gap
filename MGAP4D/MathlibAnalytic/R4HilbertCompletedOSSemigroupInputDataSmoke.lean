7f2bc032ca1b9f3628303159e3b0fe0b968a9969
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 07:21:52 +0900

    Add R4 completed OS semigroup input smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertCompletedOSSemigroupInputDataSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertCompletedOSSemigroupInputData
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertCompletedOSSemigroupInputDataSmoke : Prop := True
+
+theorem r4HilbertCompletedOSSemigroupInputDataSmoke_holds :
+    r4HilbertCompletedOSSemigroupInputDataSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
