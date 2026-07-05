9ae05ba339fec537d044f1f3afe746eb9f15333b
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 11:09:11 +0900

    Compact coordinate transport builder header for preflight

--- MGAP4D/MathlibAnalytic/EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateTransport.lean
+++ MGAP4D/MathlibAnalytic/EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateTransport.lean
@@ -115,37 +115,21 @@ structure EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransport
 namespace EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateTransportClosure
 
 /-- Build the coordinate-transport closure from coordinate cancellation. -/
-def ofCoordinateCancelClosure
-    (S : EuclideanYangMillsContinuumMeasureConstructionSpine)
-    (K : EuclideanYangMillsCompleteConstructionClosure S)
-    (R4 : EuclideanYangMillsR4GaugeFieldConstructionClosure S K)
-    (A : EuclideanYangMillsR4GaugeActionClosure S K R4)
-    (G : EuclideanYangMillsR4GaugeInvariantClosure S K R4 A)
-    (H : EuclideanYangMillsR4GaugeInvariantSchwingerClosure S K R4 A G)
-    (N : EuclideanYangMillsR4SchwingerNPointFamilyClosure S K R4 A G H)
-    (F : EuclideanYangMillsR4CorrelationFunctionalClosure S K R4 A G H N)
-    (C : EuclideanYangMillsR4CorrelationStructureClosure S K R4 A G H N F)
-    (I : EuclideanYangMillsR4ReflectionPositiveReconstructionInputClosure S K R4 A G H N F C)
-    (O : EuclideanYangMillsR4HilbertReconstructionCarrierClosure S K R4 A G H N F C I)
-    (Q : EuclideanYangMillsR4HilbertReconstructionQuotientClosure S K R4 A G H N F C I O)
-    (P : EuclideanYangMillsR4HilbertReconstructionQuotientProjectionClosure S K R4 A G H N F C I O Q)
-    (R : EuclideanYangMillsR4HilbertReconstructionQuotientRepresentativeClosure S K R4 A G H N F C I O Q P)
-    (U : EuclideanYangMillsR4HilbertReconstructionQuotientSectionClosure S K R4 A G H N F C I O Q P R)
-    (J : EuclideanYangMillsR4HilbertReconstructionQuotientSectionInjectiveClosure S K R4 A G H N F C I O Q P R U)
-    (V : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeClosure S K R4 A G H N F C I O Q P R U J)
-    (W : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeUniqueClosure S K R4 A G H N F C I O Q P R U J V)
-    (X : EuclideanYangMillsR4HilbertReconstructionQuotientMapInjectiveClosure S K R4 A G H N F C I O Q P R U J V W)
-    (Y : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeRoundTripClosure S K R4 A G H N F C I O Q P R U J V W X)
-    (Z : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRepresentativeStableClosure S K R4 A G H N F C I O Q P R U J V W X Y)
-    (T : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRepresentativeProjectionClosure S K R4 A G H N F C I O Q P R U J V W X Y Z)
-    (E : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeEquivClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T)
-    (D : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeEquivDataClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E)
-    (TR : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeEquivTransportClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D)
-    (PCL : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR)
-    (PPC : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairProjectionClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR PCL)
-    (TPC : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairTupleClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR PCL PPC)
-    (CO : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR PCL PPC TPC)
-    (CC : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateCancelClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR PCL PPC TPC CO) :
+def ofCoordinateCancelClosure (S : EuclideanYangMillsContinuumMeasureConstructionSpine) (K : EuclideanYangMillsCompleteConstructionClosure S)
+    (R4 : EuclideanYangMillsR4GaugeFieldConstructionClosure S K) (A : EuclideanYangMillsR4GaugeActionClosure S K R4)
+    (G : EuclideanYangMillsR4GaugeInvariantClosure S K R4 A) (H : EuclideanYangMillsR4GaugeInvariantSchwingerClosure S K R4 A G)
+    (N : EuclideanYangMillsR4SchwingerNPointFamilyClosure S K R4 A G H) (F : EuclideanYangMillsR4CorrelationFunctionalClosure S K R4 A G H N)
+    (C : EuclideanYangMillsR4CorrelationStructureClosure S K R4 A G H N F) (I : EuclideanYangMillsR4ReflectionPositiveReconstructionInputClosure S K R4 A G H N F C)
+    (O : EuclideanYangMillsR4HilbertReconstructionCarrierClosure S K R4 A G H N F C I) (Q : EuclideanYangMillsR4HilbertReconstructionQuotientClosure S K R4 A G H N F C I O)
+    (P : EuclideanYangMillsR4HilbertReconstructionQuotientProjectionClosure S K R4 A G H N F C I O Q) (R : EuclideanYangMillsR4HilbertReconstructionQuotientRepresentativeClosure S K R4 A G H N F C I O Q P)
+    (U : EuclideanYangMillsR4HilbertReconstructionQuotientSectionClosure S K R4 A G H N F C I O Q P R) (J : EuclideanYangMillsR4HilbertReconstructionQuotientSectionInjectiveClosure S K R4 A G H N F C I O Q P R U)
+    (V : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeClosure S K R4 A G H N F C I O Q P R U J) (W : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeUniqueClosure S K R4 A G H N F C I O Q P R U J V)
+    (X : EuclideanYangMillsR4HilbertReconstructionQuotientMapInjectiveClosure S K R4 A G H N F C I O Q P R U J V W) (Y : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeRoundTripClosure S K R4 A G H N F C I O Q P R U J V W X)
+    (Z : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRepresentativeStableClosure S K R4 A G H N F C I O Q P R U J V W X Y) (T : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRepresentativeProjectionClosure S K R4 A G H N F C I O Q P R U J V W X Y Z)
+    (E : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeEquivClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T) (D : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeEquivDataClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E)
+    (TR : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeEquivTransportClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D) (PCL : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR)
+    (PPC : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairProjectionClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR PCL) (TPC : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairTupleClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR PCL PPC)
+    (CO : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR PCL PPC TPC) (CC : EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateCancelClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR PCL PPC TPC CO) :
     EuclideanYangMillsR4HilbertReconstructionQuotientSectionRangeTransportPairCoordinateTransportClosure S K R4 A G H N F C I O Q P R U J V W X Y Z T E D TR PCL PPC TPC CO CC :=
   { quotientCoordinate_forward := EuclideanYangMillsR4HilbertReconstructionQuotient.quotientPairCoordinate_forward I TR
     rangeCoordinate_inverse := EuclideanYangMillsR4HilbertReconstructionQuotient.rangePairCoordinate_inverse I TR
