352bed4f1a6cf245a395e9519899e2c65eabc082
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 20:17:02 +0900

    Add LinearPMap graph-to-operator smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertMathlibLinearPMapSelfAdjointGraphToPMapSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertMathlibLinearPMapSelfAdjointGraphToPMap
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertMathlibLinearPMapSelfAdjointGraphToPMapSmoke : Prop := True
+
+theorem r4HilbertMathlibLinearPMapSelfAdjointGraphToPMapSmoke_holds :
+    r4HilbertMathlibLinearPMapSelfAdjointGraphToPMapSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
