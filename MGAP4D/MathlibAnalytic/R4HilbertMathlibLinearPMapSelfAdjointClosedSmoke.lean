e3d2f8cbc1795f6470d8f98daf6ae687fc3cbd27
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 19:38:54 +0900

    Add LinearPMap self-adjoint closedness smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertMathlibLinearPMapSelfAdjointClosedSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertMathlibLinearPMapSelfAdjointClosed
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertMathlibLinearPMapSelfAdjointClosedSmoke : Prop := True
+
+theorem r4HilbertMathlibLinearPMapSelfAdjointClosedSmoke_holds :
+    r4HilbertMathlibLinearPMapSelfAdjointClosedSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
