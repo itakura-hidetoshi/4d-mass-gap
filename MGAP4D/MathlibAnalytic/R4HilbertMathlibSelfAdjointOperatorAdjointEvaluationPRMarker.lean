930b6cd371678dee8cb0ba067f3c1e32a8ff4701
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 23:15:29 +0900

    Add R4 adjoint evaluation PR marker

---
+++ MGAP4D/MathlibAnalytic/R4HilbertMathlibSelfAdjointOperatorAdjointEvaluationPRMarker.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertMathlibSelfAdjointOperatorAdjointEvaluationSmoke
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertMathlibSelfAdjointOperatorAdjointEvaluationPRMarker : Prop := True
+
+theorem r4HilbertMathlibSelfAdjointOperatorAdjointEvaluationPRMarker_holds :
+    r4HilbertMathlibSelfAdjointOperatorAdjointEvaluationPRMarker :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
