fc69bf7149b436cd08bf023bca763752c4fd1e3e
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 23:16:33 +0900

    Add R4 adjoint evaluation aux

---
+++ MGAP4D/MathlibAnalytic/R4HilbertMathlibSelfAdjointOperatorAdjointEvaluationAux.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertMathlibSelfAdjointOperatorAdjointEvaluationSmoke
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertMathlibSelfAdjointOperatorAdjointEvaluationAux : Prop := True
+
+theorem r4HilbertMathlibSelfAdjointOperatorAdjointEvaluationAux_holds :
+    r4HilbertMathlibSelfAdjointOperatorAdjointEvaluationAux :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
