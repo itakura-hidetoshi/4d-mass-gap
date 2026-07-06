0b65e9f4fa325df7fcbb650f0d08b1cc723e0c95
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 23:16:07 +0900

    Add R4 adjoint evaluation note

---
+++ MGAP4D/MathlibAnalytic/R4HilbertMathlibSelfAdjointOperatorAdjointEvaluationNote.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertMathlibSelfAdjointOperatorAdjointEvaluation
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertMathlibSelfAdjointOperatorAdjointEvaluationNote : Prop := True
+
+theorem r4HilbertMathlibSelfAdjointOperatorAdjointEvaluationNote_holds :
+    r4HilbertMathlibSelfAdjointOperatorAdjointEvaluationNote :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
