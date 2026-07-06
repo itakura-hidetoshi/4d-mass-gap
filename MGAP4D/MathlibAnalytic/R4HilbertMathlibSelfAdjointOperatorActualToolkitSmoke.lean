d1e5c187b24ca473d229cbadbb56e391ec828149
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 22:18:58 +0900

    Add R4 self-adjoint operator toolkit smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertMathlibSelfAdjointOperatorActualToolkitSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertMathlibSelfAdjointOperatorActualToolkit
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertMathlibSelfAdjointOperatorActualToolkitSmoke : Prop := True
+
+theorem r4HilbertMathlibSelfAdjointOperatorActualToolkitSmoke_holds :
+    r4HilbertMathlibSelfAdjointOperatorActualToolkitSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
