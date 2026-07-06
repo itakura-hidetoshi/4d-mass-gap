4caff8399d4f81dcdc3845c50a689410f8ee50f0
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 09:00:33 +0900

    Add R4 OS generator input data (#608)
    * Add R4 OS generator input data
    
    * Add R4 OS generator input smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertOSGeneratorInputDataSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertOSGeneratorInputData
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertOSGeneratorInputDataSmoke : Prop := True
+
+theorem r4HilbertOSGeneratorInputDataSmoke_holds :
+    r4HilbertOSGeneratorInputDataSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
