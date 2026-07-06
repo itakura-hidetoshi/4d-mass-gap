308b42078499d739dc675d4e434617f13a30f224
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 16:46:59 +0900

    Add R4 mathlib spectral theorem input smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertMathlibSpectralTheoremInputSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertMathlibSpectralTheoremInput
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertMathlibSpectralTheoremInputSmoke : Prop := True
+
+theorem r4HilbertMathlibSpectralTheoremInputSmoke_holds :
+    r4HilbertMathlibSpectralTheoremInputSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
