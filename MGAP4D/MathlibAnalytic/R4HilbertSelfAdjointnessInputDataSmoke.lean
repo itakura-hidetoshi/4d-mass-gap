8e277b90c9073528f23f1c76b23fe330ca18e2b7
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 12:09:46 +0900

    Add smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertSelfAdjointnessInputDataSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertSelfAdjointnessInputData
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertSelfAdjointnessInputDataSmoke : Prop := True
+
+theorem r4HilbertSelfAdjointnessInputDataSmoke_holds :
+    r4HilbertSelfAdjointnessInputDataSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
