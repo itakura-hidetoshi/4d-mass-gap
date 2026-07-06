97c6c01e754b5ec95f28dc11f6f6ff349591d75d
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 12:08:27 +0900

    Add R4 Hamiltonian handoff API (#615)
    * Add R4 Hamiltonian handoff API
    
    * Add smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertHamiltonianHandoffAPISmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertHamiltonianHandoffAPI
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertHamiltonianHandoffAPISmoke : Prop := True
+
+theorem r4HilbertHamiltonianHandoffAPISmoke_holds :
+    r4HilbertHamiltonianHandoffAPISmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
