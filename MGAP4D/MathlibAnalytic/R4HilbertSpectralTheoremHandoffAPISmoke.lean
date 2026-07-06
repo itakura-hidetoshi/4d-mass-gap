91f9f281924699d209a2481cc0575c9383c37498
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 14:32:49 +0900

    Add smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertSpectralTheoremHandoffAPISmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertSpectralTheoremHandoffAPI
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertSpectralTheoremHandoffAPISmoke : Prop := True
+
+theorem r4HilbertSpectralTheoremHandoffAPISmoke_holds :
+    r4HilbertSpectralTheoremHandoffAPISmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
