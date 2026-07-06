036f02278160747bd348b97a3b39e392a3fb9412
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 20:34:30 +0900

    Add LinearPMap formal-adjoint smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertMathlibLinearPMapSelfAdjointFormalAdjointSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertMathlibLinearPMapSelfAdjointFormalAdjoint
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertMathlibLinearPMapSelfAdjointFormalAdjointSmoke : Prop := True
+
+theorem r4HilbertMathlibLinearPMapSelfAdjointFormalAdjointSmoke_holds :
+    r4HilbertMathlibLinearPMapSelfAdjointFormalAdjointSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
