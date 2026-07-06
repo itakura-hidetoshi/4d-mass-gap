2c5e4f55f75980f92c08e1b21fa77b4c0e4fe679
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-06 11:11:38 +0900

    Add R4 Hamiltonian input data (#612)
    * Add R4 Hamiltonian input data
    
    * Add smoke

---
+++ MGAP4D/MathlibAnalytic/R4HilbertHamiltonianInputDataSmoke.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertHamiltonianInputData
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+def r4HilbertHamiltonianInputDataSmoke : Prop := True
+
+theorem r4HilbertHamiltonianInputDataSmoke_holds :
+    r4HilbertHamiltonianInputDataSmoke :=
+  trivial
+
+end
+
+end MathlibAnalytic
+end MGAP4D
