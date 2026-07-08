c68712d9bfc0105121005633958be5b8c58c59a2
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-09 08:28:10 +0900

    Add direct actual bounded final import

---
+++ MGAP4D/MathlibAnalytic/R4HilbertMathlibSelfAdjointOperatorDirectActualBoundedFinal.lean
@@ -0,0 +1,17 @@
+import MGAP4D.MathlibAnalytic.R4HilbertMathlibSelfAdjointOperatorDirectActualBoundedTop
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+namespace EuclideanYangMillsR4HilbertReconstructionQuotient
+
+/-! Final aggregate import for direct actual boundedness. -/
+
+end EuclideanYangMillsR4HilbertReconstructionQuotient
+
+end
+
+end MathlibAnalytic
+end MGAP4D
