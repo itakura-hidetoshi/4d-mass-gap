5bbfdfc5de4bbea8fbbcaa96dd0c0fbee2685374
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-28 07:38:49 +0900

    Fix finite marginal compile-smoke restriction type

--- MGAP4D/MathlibAnalytic/LinearMarkovIntegerFiniteMarginalPMFCompileSmoke.lean
+++ MGAP4D/MathlibAnalytic/LinearMarkovIntegerFiniteMarginalPMFCompileSmoke.lean
@@ -14,7 +14,7 @@ example {Ω : Type*} [Fintype Ω]
     (I J : Finset ℤ) (hJI : J ⊆ I) :
     linearMarkovIntegerFiniteMarginalPMF initial transition J =
       (linearMarkovIntegerFiniteMarginalPMF initial transition I).map
-        (Finset.restrict₂ hJI) :=
+        (@Finset.restrict₂ ℤ (fun _ => Ω) J I hJI) :=
   linearMarkovIntegerFiniteMarginalPMF_projective
     initial transition hdb I J hJI
 
