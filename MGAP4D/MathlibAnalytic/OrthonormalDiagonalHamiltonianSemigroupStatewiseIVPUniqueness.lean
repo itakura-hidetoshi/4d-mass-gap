e2062c9004a7b62d094e2e3e4a2ff171e3133719
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-29 19:57:03 +0900

    Start generic statewise IVP uniqueness layer

---
+++ MGAP4D/MathlibAnalytic/OrthonormalDiagonalHamiltonianSemigroupStatewiseIVPUniqueness.lean
@@ -0,0 +1,12 @@
+import MGAP4D.MathlibAnalytic.OrthonormalDiagonalHamiltonianSemigroupOperatorExp
+import Mathlib.Analysis.Calculus.MeanValue
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+end
+
+end MathlibAnalytic
+end MGAP4D
