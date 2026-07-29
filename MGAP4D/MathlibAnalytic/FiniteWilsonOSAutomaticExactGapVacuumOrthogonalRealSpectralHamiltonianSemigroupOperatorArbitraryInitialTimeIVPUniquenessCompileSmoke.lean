c7abb6ee147cb10fdad0eb83b92fb73cc1df3422
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-29 20:22:47 +0900

    Add compile smoke for arbitrary-time operator IVP uniqueness

---
+++ MGAP4D/MathlibAnalytic/FiniteWilsonOSAutomaticExactGapVacuumOrthogonalRealSpectralHamiltonianSemigroupOperatorArbitraryInitialTimeIVPUniquenessCompileSmoke.lean
@@ -0,0 +1,10 @@
+import MGAP4D.MathlibAnalytic.FiniteWilsonOSAutomaticExactGapVacuumOrthogonalRealSpectralHamiltonianSemigroupOperatorArbitraryInitialTimeIVPUniqueness
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+-- Compile-smoke import for the generic, constructed, and physical left/right
+-- arbitrary-initial-time operator-norm IVP uniqueness chain.
+
+end MathlibAnalytic
+end MGAP4D
