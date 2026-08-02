c3f82887448843a3ffe20f603a9e626730904102
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-08-02 14:19:13 +0900

    Add dependent Pi block hierarchy final package

---
+++ MGAP4D/MathlibAnalytic/ContinuousLinearMapFiniteDimensionalRealResolventJointParameterRemainderBanachDependentPiBlockHierarchyToleranceFinalPackage.lean
@@ -0,0 +1,8 @@
+import MGAP4D.MathlibAnalytic.ContinuousLinearMapFiniteDimensionalRealResolventJointParameterRemainderBanachDependentPiBlockHierarchyToleranceCertificatePackage
+
+/-!
+# Dependent Pi block hierarchy tolerance final package
+
+Final generic package root for refinement contractions, hierarchy master
+comparison, and seven-channel remainder certificates.
+-/
