9d803c65fab34363be5e415b84d44edbb28218f2
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-08-02 15:10:50 +0900

    Add finite rooted hierarchy certificate package

---
+++ MGAP4D/MathlibAnalytic/ContinuousLinearMapFiniteDimensionalRealResolventJointParameterRemainderBanachDependentPiFiniteRootedBlockHierarchyToleranceCertificatePackage.lean
@@ -0,0 +1,8 @@
+import MGAP4D.MathlibAnalytic.ContinuousLinearMapFiniteDimensionalRealResolventJointParameterRemainderBanachDependentPiFiniteRootedBlockHierarchyToleranceAggregate
+
+/-!
+# Dependent Pi finite rooted block hierarchy certificate package
+
+Single import root for the rooted hierarchy master and its fixed-parameter,
+compact-uniform, and arbitrary-joint-net closed-box certificates.
+-/
