d1ea97dd39555ee63ef449f1182c99fa38dc5f81
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-08-03 14:38:49 +0900

    Add single CI target for one-slab transfer package

---
+++ MGAP4D/MathlibAnalytic/Z2FiniteEvenFourTorusTemporalGaugeOneSlabTransferPackageAll.lean
@@ -0,0 +1,12 @@
+import MGAP4D.MathlibAnalytic.Z2FiniteEvenFourTorusTemporalGaugeOneSlabREADME
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+/-- Single import target for CI and downstream developments. -/
+theorem z2FiniteEvenFourTorusTemporalGaugeOneSlabTransferPackageAll_loaded :
+    True :=
+  trivial
+
+end MathlibAnalytic
+end MGAP4D
