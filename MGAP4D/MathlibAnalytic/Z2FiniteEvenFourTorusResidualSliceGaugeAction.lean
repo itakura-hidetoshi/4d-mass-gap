7b88be18ce353862688d19424f77576465228e24
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-08-03 16:45:02 +0900

    Cancel commutative plaquette gauge factors explicitly

--- MGAP4D/MathlibAnalytic/Z2FiniteEvenFourTorusResidualSliceGaugeAction.lean
+++ MGAP4D/MathlibAnalytic/Z2FiniteEvenFourTorusResidualSliceGaugeAction.lean
@@ -93,7 +93,20 @@ theorem commGroupPlaquetteGaugeCancellation
         (g01 * c * g11⁻¹)⁻¹ *
         (g00 * d * g01⁻¹)⁻¹ =
       a * b * c⁻¹ * d⁻¹ := by
-  group
+  simp only [mul_inv_rev, inv_inv]
+  calc
+    (g00 * a * g10⁻¹) *
+          (g10 * b * g11⁻¹) *
+          (g11 * c⁻¹ * g01⁻¹) *
+          (g01 * d⁻¹ * g00⁻¹) =
+        (g00 * g00⁻¹) *
+          (g10⁻¹ * g10) *
+          (g11⁻¹ * g11) *
+          (g01⁻¹ * g01) *
+          (a * b * c⁻¹ * d⁻¹) := by
+      ac_rfl
+    _ = a * b * c⁻¹ * d⁻¹ := by
+      simp
 
 /-- Spatial plaquette holonomy is invariant under residual slice gauge
 transformations. -/
