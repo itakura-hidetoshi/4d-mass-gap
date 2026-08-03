a10ae24900fb0214ca339b8fc5de2a55f7be1aae
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-08-03 20:24:47 +0900

    Fix classical proposition decidability instance

--- MGAP4D/MathlibAnalytic/Z2FiniteEvenFourTorusGaussProjectedOneSlabNontriviality.lean
+++ MGAP4D/MathlibAnalytic/Z2FiniteEvenFourTorusGaussProjectedOneSlabNontriviality.lean
@@ -8,7 +8,7 @@ open scoped BigOperators InnerProduct
 
 noncomputable section
 
-local instance (p : Prop) : Decidable p := Classical.propComplete p
+local instance (p : Prop) : Decidable p := Classical.propDecidable p
 
 /-- Indicator of the finite gauge orbit containing a boundary configuration. -/
 noncomputable def finiteGroupOrbitIndicator
