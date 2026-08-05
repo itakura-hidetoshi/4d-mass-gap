c04c72d0029d9713daf85ecd0329a937a50d23a9
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-08-05 22:50:10 +0900

    Fix real inverse continuity and explicit matrix-vector notation

--- MGAP4D/MathlibAnalytic/FiniteContinuousNonsingularMatrixInverse.lean
+++ MGAP4D/MathlibAnalytic/FiniteContinuousNonsingularMatrixInverse.lean
@@ -22,7 +22,7 @@ theorem continuous_matrix_inv_of_det_ne_zero
   intro x
   exact
     (continuousAt_matrix_inv (A x)
-      (continuousAt_inv₀.mpr (hdet x))).comp x hA.continuousAt
+      (continuousAt_inv₀ (hdet x))).comp x hA.continuousAt
 
 /-- Applying a continuous nonsingular inverse matrix family to a continuous
 finite vector family remains continuous. -/
@@ -36,7 +36,7 @@ theorem continuous_matrix_inv_mulVec_of_det_ne_zero
     (hA : Continuous A)
     (hv : Continuous v)
     (hdet : ∀ x, (A x).det ≠ 0) :
-    Continuous (fun x => (A x)⁻¹ *ᵥ v x) :=
+    Continuous (fun x => Matrix.mulVec (A x)⁻¹ (v x)) :=
   (continuous_matrix_inv_of_det_ne_zero A hA hdet).matrix_mulVec hv
 
 end
