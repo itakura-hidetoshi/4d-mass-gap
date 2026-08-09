dc2778f2fd088d4c4b3729c6d47f75840f0ff192
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-08-10 00:14:30 +0900

    Remove no-op slow-state simplification

--- MGAP4D/MathlibAnalytic/PhysicalYangMillsWilsonScalarTwoStepReverseMass.lean
+++ MGAP4D/MathlibAnalytic/PhysicalYangMillsWilsonScalarTwoStepReverseMass.lean
@@ -238,7 +238,6 @@ theorem embeddedSlowState_twoStepDefectRate_lt_dominator
     V.twoStepDefectRate_le_finiteEnergy_add_excess
       n (V.finiteSlowState n) (V.finiteSlowState_norm n)
   have hslow := V.finiteSlowState_energy_lt_rate_add_spacing n
-  dsimp [embeddedSlowState] at hcompat
   unfold slowStateDominator
   linarith
 
