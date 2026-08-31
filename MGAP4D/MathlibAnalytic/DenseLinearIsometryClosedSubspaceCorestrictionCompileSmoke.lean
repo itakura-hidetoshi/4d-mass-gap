0c65fee131904c791e65a721709d60ee1de0a685
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-01 07:43:56 +0900

    Add closed-subspace dense-core compile smoke

---
+++ MGAP4D/MathlibAnalytic/DenseLinearIsometryClosedSubspaceCorestrictionCompileSmoke.lean
@@ -0,0 +1,20 @@
+import MGAP4D.MathlibAnalytic.DenseLinearIsometryClosedSubspaceCorestriction
+
+namespace MGAP4D
+namespace MathlibAnalytic
+
+noncomputable section
+
+example
+    {C E : Type*}
+    [NormedAddCommGroup C] [NormedSpace ℝ C]
+    [NormedAddCommGroup E] [NormedSpace ℝ E]
+    {S : Submodule ℝ E}
+    (R : RealHilbertClosedSubspaceDenseCoreRealization S) :
+    DenseRange R.corestrict :=
+  R.corestrict_denseRange
+
+end
+
+end MathlibAnalytic
+end MGAP4D
