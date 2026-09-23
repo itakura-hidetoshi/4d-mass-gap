31a593b1de4bf1e5e0e2ace5b45b41092837ac77
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-23 20:50:53 +0900

    Restore condExp transport compile smoke

--- MGAP4D/MathlibAnalytic/PeriodicHypercubicEvenSpecialUnitaryPhysicalTransferBetaZeroGroundStateSixSpatialGenuinePairHaarTransportCompileSmoke.lean
+++ MGAP4D/MathlibAnalytic/PeriodicHypercubicEvenSpecialUnitaryPhysicalTransferBetaZeroGroundStateSixSpatialGenuinePairHaarTransportCompileSmoke.lean
@@ -5,6 +5,7 @@ namespace MGAP4D.MathlibAnalytic
 noncomputable section
 
 #check realL2CastOfMeasureEq
+#check realL2CastOfMeasureEq_condExpL2
 #check realL2CastOfMeasureEq_compMeasurePreserving
 
 #check
